94ca40764161d68a46610e5ed1e81c84b5d34317
Geovani Figueiredo <geovani537@gmail.com>
2025-03-25 18:11:03 +0000

    minor ajust

--- inc/actions.php
+++ inc/actions.php
@@ -24,7 +24,7 @@ add_action('bp_member_header_actions', 'hide_bp_edit_profile_button', 1);
 add_action('bp_actions', 'intraner_fafar_block_for_not_profile_owners');
 
 // Bloquear todo mundo de qualquer coisa
-// add_action('template_redirect', 'redirect_all_users');
+add_action('template_redirect', 'redirect_all_users');
 
 function restrict_bp_profile_editing() {
     if (!current_user_can('manage_options') && bp_is_user_profile_edit()) {
@@ -65,7 +65,7 @@ function redirect_all_users() {
         $current_user_id = get_current_user_id();
         
         // If user is not ID 23, redirect them
-        if ( in_array( $current_user_id, [1, 2078, 2041] ) ) {
+        if ( ! in_array( $current_user_id, [1, 2078, 2041] ) ) {
             wp_redirect('https://intranet.farmacia.ufmg.br/wp-login.php?redirect_to=https%3A%2F%2Fintranet.farmacia.ufmg.br%2F'); // Change to your desired redirect URL
             exit;
         }
