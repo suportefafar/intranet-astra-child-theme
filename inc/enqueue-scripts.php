a5906adcb1e17cf6699e291ab9f191712a5f077c
Geovani Figueiredo <geovani537@gmail.com>
2025-03-26 17:32:16 +0000

    change Grid.js CDN

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -47,7 +47,7 @@ function add_header_custom_scripts() {
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
 
-        <link href="https://unpkg.com/gridjs/dist/theme/mermaid.min.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css" rel="stylesheet" />
 	<?php
 
 }
@@ -67,7 +67,7 @@ function add_footer_custom_scripts() {
         
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-        <script src="https://unpkg.com/gridjs/dist/gridjs.umd.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/gridjs/dist/gridjs.umd.js"></script>
 		
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.14.4/dist/sweetalert2.all.min.js"></script>
 
