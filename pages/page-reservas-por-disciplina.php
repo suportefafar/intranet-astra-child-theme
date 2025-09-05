3cb5d0cb43ae64f84ff13974dcb2e11a2c1aaeae
Geovani Figueiredo <geovani537@gmail.com>
2025-09-05 16:55:31 -0300

    add: choose class subject time for new reservation, and redirect these informations to reservation assistant

--- pages/page-reservas-por-disciplina.php
+++ pages/page-reservas-por-disciplina.php
@@ -58,6 +58,51 @@ function parse_weekday_to_name( $number ) {
 	)[ (int) $number ] ?? '--';
 }
 
+function parse_subject_desired_time( $input ) {
+	$result = [];
+	preg_match_all( '/(\d{1,2}:\d{2})\s+(\d{1,2}:\d{2})\s+\((\w{3})\)/', $input, $matches, PREG_SET_ORDER );
+
+	// Mapeamento dos dias da semana para números (Seg = 1, Ter = 2, ..., Dom = 7)
+	$days_map = [ 
+		'SEG' => 1, 'TER' => 2, 'QUA' => 3,
+		'QUI' => 4, 'SEX' => 5, 'SAB' => 6, 'DOM' => 7
+	];
+
+	foreach ( $matches as $match ) {
+		$start = $match[1];
+		$end = $match[2];
+		$weekday = $days_map[ strtoupper( $match[3] ) ] ?? null;
+
+		if ( $weekday ) {
+			$result[] = [ 
+				'start' => $start,
+				'end' => $end,
+				'weekday' => [ (int) $weekday ]
+			];
+		}
+	}
+
+	return $result;
+}
+
+function get_weekday_label( $weekday_num ) {
+	$weekday_num = intval( $weekday_num );
+
+	$weekdays_label = [
+		'DOM',
+		'SEG',
+		'TER',
+		'QUA',
+		'QUI',
+		'SEX',
+		'SAB'
+	];
+
+	if ( isset( $weekdays_label[ $weekday_num ] ) )
+		return $weekdays_label[ $weekday_num ];
+
+	return '';
+}
 
 get_header(); ?>
 
@@ -82,44 +127,99 @@ get_header(); ?>
 -->
 
 	<!-- TABLES -->
-	<h4>Reservas para <?= ( $class_subject_title ?? '--' ) ?></h4>
-	<h5><?= ( $reservations_by_subject ? count( $reservations_by_subject ) : '0' ) ?> reserva(s) encontrada(s)</h5>
-	<table class="table">
-		<thead>
-			<tr>
-				<th scope="col">Sala</th>
-				<th scope="col">Início</th>
-				<th scope="col">Fim</th>
-				<th scope="col">Dia da semana</th>
-				<th scope="col">Ações</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-			if ( count( $reservations_by_subject ) > 0 ) :
-				foreach ( $reservations_by_subject as $reservation ) :
-					?>
-					<tr>
-						<td><?= $reservation['data']['place']['data']['number'] ?></td>
-						<td><?= $reservation['data']['start_time'] ?></td>
-						<td><?= $reservation['data']['end_time'] ?></td>
-						<td><?= implode( ', ', array_map( function ($weekday) {
-							return parse_weekday_to_name( $weekday ); }, (array) $reservation['data']['weekdays'] ) ) ?>
-						</td>
-						<td>
-							<a class="btn btn-outline-secondary" href="/visualizar-reserva/?id=<?= $reservation['id'] ?>"
-								target="blank" title="Detalhes">
-								<i class="bi bi-info-lg"></i>
-							</a>
-						</td>
-					</tr>
-
+	<div class="d-flex flex-column gap-2">
+		<h5 class="m-0"><?= ( $class_subject_title ?? '--' ) ?></h5>
+		<div class="d-flex gap-1 justify-content-between align-items-end">
+			<span><?= ( $reservations_by_subject ? count( $reservations_by_subject ) : '0' ) ?> reserva(s) encontrada(s)</span>
+			
+			<button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#intranetFafarSujectClassTimesModal">
+				<i class="bi bi-plus-lg"></i>
+				Reservar Disciplina
+			</button>
+		</div>
+		<table class="table">
+			<thead>
+				<tr>
+					<th scope="col">Sala</th>
+					<th scope="col">Início</th>
+					<th scope="col">Fim</th>
+					<th scope="col">Dia da semana</th>
+					<th scope="col">Ações</th>
+				</tr>
+			</thead>
+			<tbody>
 				<?php
-				endforeach;
-			endif;
-			?>
-		</tbody>
-	</table>
+				if ( count( $reservations_by_subject ) > 0 ) :
+					foreach ( $reservations_by_subject as $reservation ) :
+						?>
+						<tr>
+							<td><?= $reservation['data']['place']['data']['number'] ?></td>
+							<td><?= $reservation['data']['start_time'] ?></td>
+							<td><?= $reservation['data']['end_time'] ?></td>
+							<td><?= implode( ', ', array_map( function ($weekday) {
+								return parse_weekday_to_name( $weekday ); }, (array) $reservation['data']['weekdays'] ) ) ?>
+							</td>
+							<td>
+								<a class="btn btn-outline-secondary" href="/visualizar-reserva/?id=<?= $reservation['id'] ?>"
+									target="blank" title="Detalhes">
+									<i class="bi bi-info-lg"></i>
+								</a>
+							</td>
+						</tr>
+
+					<?php
+					endforeach;
+				endif;
+				?>
+			</tbody>
+		</table>
+	</div>
+
+	<!-- Modal para selecionar o horário da nova reserva da disciplina -->
+	<div class="modal fade" id="intranetFafarSujectClassTimesModal" tabindex="-1"
+		aria-labelledby="intranetFafarSujectClassTimesModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h1 class="modal-title fs-5" id="intranetFafarSujectClassTimesModalLabel">Horários</h1>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					<table class="table border border-start-0 border-end-0">
+						<tbody>
+							<?php
+								$times = parse_subject_desired_time( $class_subject['data']['desired_time'] );
+								if ( ! empty( $times ) ):
+									foreach( $times as $time ): 
+										$capacity      = $class_subject['data']['number_vacancies_offered'] ?? 0;
+										$weekday       = $time['weekday'][0];
+										$weekday_label = get_weekday_label( $weekday );
+										$time_display  = $time['start'] . ' ' . $time['end'] . ' (' . $weekday_label . ')';
+							?>
+							<tr>
+								<td scope="col"><?= $time_display ?></td>
+								<td scope="col">
+									<a href="/assistente-de-reservas-de-salas/?subject=<?= $class_subject['id'] ?>&capacity=<?= $capacity ?>&start_time=<?= $time['start'] ?>&end_time=<?= $time['end'] ?>&weekdays=<?= $weekday ?>&frequency=weekly" class="btn btn-outline-primary" target="_blank" title="Reservar para <?= $time_display ?>">
+										<i class="bi bi-arrow-right"></i>
+									</a>
+								</td>
+							</tr>
+							<?php 	
+									endforeach;
+								else: 
+							?>
+							<tr>
+								<td scope="col">Adicione um horário.</td>
+							</tr>
+							<?php 
+								endif; 
+							?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
 
 	<?php
 	$user_role = intranet_fafar_get_user_slug_role();
