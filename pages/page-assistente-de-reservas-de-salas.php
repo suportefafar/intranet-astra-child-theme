39c601bfa70ebb732480d5d631b9ef090ecf58fa
Geovani Figueiredo <geovani537@gmail.com>
2026-04-07 11:33:55 -0300

    refactor: standardize PHP code style and spacing across reservation pages and custom sniffs

--- pages/page-assistente-de-reservas-de-salas.php
+++ pages/page-assistente-de-reservas-de-salas.php
@@ -8,16 +8,18 @@
  * @since 1.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
-function fafar_intranet_format_date_local( $dt ) {
-	return DateTime::createFromFormat( 'Y-m-d', $dt )->format( 'd/m/Y' );
+function fafar_intranet_format_date_local($dt)
+{
+	return DateTime::createFromFormat('Y-m-d', $dt)->format('d/m/Y');
 }
 
-function fafar_intranet_get_frequency_display_text( $f ) {
-	switch ( $f ) {
+function fafar_intranet_get_frequency_display_text($f)
+{
+	switch ($f) {
 		case 'once':
 			return 'Única';
 		case 'daily':
@@ -31,26 +33,28 @@ function fafar_intranet_get_frequency_display_text( $f ) {
 	}
 }
 
-function fafar_intranet_get_weekdays( $wds ) {
-	$weekdays = array( 'Domingo',
+function fafar_intranet_get_weekdays($wds)
+{
+	$weekdays = array(
+		'Domingo',
 		'Segunda',
 		'Terça',
 		'Quarta',
 		'Quinta',
 		'Sexta',
-		'Sábado' );
+		'Sábado'
+	);
 
-	$weekdays_arr = array_map( function ($wd) use ($weekdays) {
-		return $weekdays[ $wd ];
-	}, $wds );
+	$weekdays_arr = array_map(function ($wd) use ($weekdays) {
+		return $weekdays[$wd];
+	}, $wds);
 
-	return implode( ', ', $weekdays_arr );
+	return implode(', ', $weekdays_arr);
 }
-
 
 get_header(); ?>
 
-<?php if ( astra_page_layout() == 'left-sidebar' ) : ?>
+<?php if (astra_page_layout() == 'left-sidebar'): ?>
 
 	<?php get_sidebar(); ?>
 
@@ -70,150 +74,150 @@ get_header(); ?>
 	* Início
 -->
 
-	<?php if ( isset( $_GET['step'] ) && $_GET['step'] === '3' ) : ?>
+	<?php if (isset($_GET['step']) && $_GET['step'] === '3'): ?>
 		<h5>3/3 - Confirmar dados</h5>
 		<br />
 		<table class="table border border-start-0 border-end-0">
 			<tbody>
 				<tr>
 					<td>Descrição</td>
-      				<th scope="row"><?= esc_html( $_POST['desc'] ) ?></th>
+					<th scope="row"><?= esc_html($_POST['desc']) ?></th>
 				</tr>
 				<tr>
 					<td>Disciplina</th>
-					<?php
-						$class_subject = intranet_fafar_api_get_submission_by_id( sanitize_text_field( $_POST['class_subject'] ) );
+						<?php
+						$class_subject = intranet_fafar_api_get_submission_by_id(sanitize_text_field($_POST['class_subject']));
 
 						$class_subject_display = '';
-						if ( ! empty( $class_subject ) ) {
+						if (!empty($class_subject)) {
 							$class_subject_display = $class_subject['data']['code'] . ' (' . $class_subject['data']['group'] . ')';
 						}
-					?>
-      				<th scope="row"><?= $class_subject_display ?></td>
+						?>
+					<th scope="row"><?= $class_subject_display ?></td>
 				</tr>
 				<tr>
 					<?php
-						$date_display = fafar_intranet_format_date_local( esc_html( $_POST['date'] ) );
+					$date_display = fafar_intranet_format_date_local(esc_html($_POST['date']));
 					?>
-					<td><?= ( sanitize_text_field( $_POST['frequency'] ) === 'once' ? 'Data' : 'Data Inicial' ) ?></td>
-      				<th scope="row"><?= $date_display ?></th>
+					<td><?= (sanitize_text_field($_POST['frequency']) === 'once' ? 'Data' : 'Data Inicial') ?></td>
+					<th scope="row"><?= $date_display ?></th>
 				</tr>
 				<?php
-					$end_date_display = null;
-					if ( $_POST['end_date'] ) {
-						$end_date_display = fafar_intranet_format_date_local( esc_html( $_POST['end_date'] ) );
-					}
+				$end_date_display = null;
+				if ($_POST['end_date']) {
+					$end_date_display = fafar_intranet_format_date_local(esc_html($_POST['end_date']));
+				}
 				?>
 				<tr>
 					<td>Data Final</td>
-      				<th scope="row"><?= ( $end_date_display ?? '' ) ?></th>
+					<th scope="row"><?= ($end_date_display ?? '') ?></th>
 				</tr>
 				<tr>
 					<td>Hora Início</td>
-      				<th scope="row"><?= esc_html( $_POST['start_time'] ) ?></th>
+					<th scope="row"><?= esc_html($_POST['start_time']) ?></th>
 				</tr>
 				<tr>
 					<td>Hora Fim</td>
-      				<th scope="row"><?= esc_html( $_POST['end_time'] ) ?></th>
+					<th scope="row"><?= esc_html($_POST['end_time']) ?></th>
 				</tr>
 				<tr>
 					<td>Frequência</td>
 					<?php
-						$frequency_sanitized = sanitize_text_field( $_POST['frequency'] );
-						$frequency_display   = fafar_intranet_get_frequency_display_text( $frequency_sanitized );
+					$frequency_sanitized = sanitize_text_field($_POST['frequency']);
+					$frequency_display = fafar_intranet_get_frequency_display_text($frequency_sanitized);
 					?>
 					<th scope="row"><?= $frequency_display ?></th>
 				</tr>
 				<tr>
 					<td>Dias da Semana</td>
 					<?php
-						$weekdays_sanitized = sanitize_text_field( $_POST['weekdays'] );
-						$weekdays_arr       = explode( ',', $weekdays_sanitized );
-						$weekdays_display   = fafar_intranet_get_weekdays( $weekdays_arr );
+					$weekdays_sanitized = sanitize_text_field($_POST['weekdays']);
+					$weekdays_arr = explode(',', $weekdays_sanitized);
+					$weekdays_display = fafar_intranet_get_weekdays($weekdays_arr);
 					?>
-      				<th scope="row"><?= $weekdays_display ?></th>
+					<th scope="row"><?= $weekdays_display ?></th>
 				</tr>
 				<tr>
 					<td>Local</td>
 					<?php
-						$place = intranet_fafar_api_get_submission_by_id( sanitize_text_field( $_POST['place'] ) );
+					$place = intranet_fafar_api_get_submission_by_id(sanitize_text_field($_POST['place']));
 
-						$place_display = '';
-						if ( ! empty( $place ) ) {
-							$place_display = $place['data']['number'];
-						}
+					$place_display = '';
+					if (!empty($place)) {
+						$place_display = $place['data']['number'];
+					}
 					?>
-      				<th scope="row"><?= $place_display ?></th>
+					<th scope="row"><?= $place_display ?></th>
 				</tr>
 				<tr>
 					<td>Solicitante</td>
 					<?php
-						$applicant_sanitized = sanitize_text_field( $_POST['applicant'] );
-						$applicant           = get_userdata( intval( $applicant_sanitized ) );
+					$applicant_sanitized = sanitize_text_field($_POST['applicant']);
+					$applicant = get_userdata(intval($applicant_sanitized));
 
-						$applicant_display = '';
-						if ( ! empty( $applicant ) ) {
-							$applicant_display = $applicant->data->display_name;
-						}
+					$applicant_display = '';
+					if (!empty($applicant)) {
+						$applicant_display = $applicant->data->display_name;
+					}
 					?>
-      				<th scope="row"><?= $applicant_display ?></th>
+					<th scope="row"><?= $applicant_display ?></th>
 				</tr>
 			</tbody>
 		</table>
-		<?= do_shortcode( '[contact-form-7 id="c567fc2" title="Adicionar Reserva Pelo Assistente"]' ) ?>
-	<?php elseif ( isset( $_GET['step'] ) && $_GET['step'] === '2' ) : ?>
+		<?= do_shortcode('[contact-form-7 id="c567fc2" title="Adicionar Reserva Pelo Assistente"]') ?>
+	<?php elseif (isset($_GET['step']) && $_GET['step'] === '2'): ?>
 
 		<h5>2/3 - Informações adicionais</h5>
-		
+
 		<form action="/assistente-de-reservas-de-salas?step=3" method="post" class="mb-5">
 
-			<input type="hidden" name="date" value="<?= esc_html( $_POST['date'] ) ?>" />
-			<input type="hidden" name="start_time" value="<?= esc_html( $_POST['start_time'] ) ?>" />
-			<input type="hidden" name="end_time" value="<?= esc_html( $_POST['end_time'] ) ?>" />
-			<input type="hidden" name="place" value="<?= esc_html( $_POST['place'] ) ?>" />
-			<input type="hidden" name="frequency" value="<?= esc_html( $_POST['frequency'] ) ?>" />
-			<?php 
-				$weekdays_as_str = '';
-				if ( isset( $_POST['weekdays'] ) ) {
-					$sanitized_weekdays_array = array_map( 'sanitize_text_field', $_POST['weekdays'] );
-					$weekdays_as_str = implode( ',', $sanitized_weekdays_array ); 
-				}
+			<input type="hidden" name="date" value="<?= esc_html($_POST['date']) ?>" />
+			<input type="hidden" name="start_time" value="<?= esc_html($_POST['start_time']) ?>" />
+			<input type="hidden" name="end_time" value="<?= esc_html($_POST['end_time']) ?>" />
+			<input type="hidden" name="place" value="<?= esc_html($_POST['place']) ?>" />
+			<input type="hidden" name="frequency" value="<?= esc_html($_POST['frequency']) ?>" />
+			<?php
+			$weekdays_as_str = '';
+			if (isset($_POST['weekdays'])) {
+				$sanitized_weekdays_array = array_map('sanitize_text_field', $_POST['weekdays']);
+				$weekdays_as_str = implode(',', $sanitized_weekdays_array);
+			}
 			?>
 			<input type="hidden" name="weekdays" value="<?= $weekdays_as_str ?>" />
-			<input type="hidden" name="end_date" value="<?= esc_html( $_POST['end_date'] ) ?>" />
-			
+			<input type="hidden" name="end_date" value="<?= esc_html($_POST['end_date']) ?>" />
+
 			<div class="input-group mb-3">
 				<label for="desc">Descrição</label>
 				<input type="text" name="desc" id="desc" />
 			</div>
-			
-			<?php 
-				if ( isset( $_POST['class_subject'] ) ) : 
-			?>
-				<input type="hidden" name="class_subject" value="<?= esc_html( $_POST['class_subject'] ) ?>" />
+
 			<?php
-				else:
-			?>
+			if (isset($_POST['class_subject'])):
+				?>
+				<input type="hidden" name="class_subject" value="<?= esc_html($_POST['class_subject']) ?>" />
+				<?php
+			else:
+				?>
 				<!-- Disciplina
 				[select class_subject include_blank far_crud_shortcode:intranet_fafar_get_subjects_as_select_options] -->
 				<div class="form-group mb-3">
 					<label for="class-subject">Disciplina</label>
 					<select class="form-select" aria-label="Class subject select" id="class-subject" name="class_subject">
 						<?php
-							$class_subjects = intranet_fafar_get_subjects_as_select_options( false );
+						$class_subjects = intranet_fafar_get_subjects_as_select_options(false);
 
-							if ( ! empty( $class_subjects ) ) :
-								foreach( $class_subjects as $key => $value ) :
-						?>
-									<option value="<?= $key ?>"><?= $value ?></option>
-						<?php
-								endforeach;
-							endif;
+						if (!empty($class_subjects)):
+							foreach ($class_subjects as $key => $value):
+								?>
+								<option value="<?= $key ?>"><?= $value ?></option>
+								<?php
+							endforeach;
+						endif;
 						?>
 					</select>
 				</div>
-			<?php
-				endif;
+				<?php
+			endif;
 			?>
 
 			<!-- Solicitante
@@ -222,36 +226,36 @@ get_header(); ?>
 				<label for="applicant">Solicitante</label>
 				<select class="form-select" aria-label="Class applicant" id="applicant" name="applicant">
 					<?php
-						$applicants = intranet_fafar_get_users_as_select_options( false );
-						if ( ! empty( $applicants ) ) :
-							foreach( $applicants as $key => $value ) :
-					?>
-								<option value="<?= $key ?>"><?= $value ?></option>
-					<?php
-							endforeach;
-						endif;
+					$applicants = intranet_fafar_get_users_as_select_options(false);
+					if (!empty($applicants)):
+						foreach ($applicants as $key => $value):
+							?>
+							<option value="<?= $key ?>"><?= $value ?></option>
+							<?php
+						endforeach;
+					endif;
 					?>
 				</select>
 			</div>
-			
+
 			<button type="submit" class="btn btn-secondary">Continuar</button>
 		</form>
 
-	<?php else : ?>
+	<?php else: ?>
 		<a href="/sugestao-de-reservas-de-salas" class="btn btn-secondary mb-3">
 			<i class="bi bi-bookmark-plus"></i>
 			Sugestões
 		</a>
-		
+
 		<h5>1/3 - Informações básicas</h5>
-		
+
 		<form action="/assistente-de-reservas-de-salas?step=2" method="post" class="mb-5" id="form-step-1">
 			<input type="hidden" name="place" value="" />
 			<input type="hidden" name="class_subject" value="" />
 			<div class="form-group mb-3">
 				<label for="event_date">* Data </label>
-				<input type="date" class="form-control" id="date" name="date" min="2024-09-10" 
-					aria-required="true" required />
+				<input type="date" class="form-control" id="date" name="date" min="2024-09-10" aria-required="true"
+					required />
 			</div>
 			<div class="form-group mb-3">
 				<label for="start_time">* Início </label>
@@ -265,7 +269,8 @@ get_header(); ?>
 				<label>Frequência</label>
 				<div class="d-flex gap-3">
 					<div class="form-check">
-						<input class="form-check-input" type="radio" name="frequency" value="once" id="radioFrequency1" checked>
+						<input class="form-check-input" type="radio" name="frequency" value="once" id="radioFrequency1"
+							checked>
 						<label class="form-check-label" for="radioFrequency1">
 							Uma vez
 						</label>
@@ -282,7 +287,7 @@ get_header(); ?>
 				<label for="container-weekdays">Dia da semana</label>
 				<div class="d-flex gap-3">
 					<div class="form-check">
-						<input class="form-check-input" type="checkbox" value="1" name="weekdays[]"  id="checkWeekdays1">
+						<input class="form-check-input" type="checkbox" value="1" name="weekdays[]" id="checkWeekdays1">
 						<label class="form-check-label" for="checkWeekdays1">
 							Segunda
 						</label>
@@ -321,7 +326,7 @@ get_header(); ?>
 			</div>
 			<div class="mb-3" id="container-end-date">
 				<label for="end_date" class="form-label">Data de término</label>
-				<input type="date" class="form-control" name="end_date" id="end_date" /> 
+				<input type="date" class="form-control" name="end_date" id="end_date" />
 			</div>
 			<div class="form-group mb-3">
 				<label for="capacity">* Capacidade </label>
@@ -353,7 +358,7 @@ get_header(); ?>
 
 </div><!-- #primary -->
 
-<?php if ( astra_page_layout() == 'right-sidebar' ) : ?>
+<?php if (astra_page_layout() == 'right-sidebar'): ?>
 
 	<?php get_sidebar(); ?>
 
